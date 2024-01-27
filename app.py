cad91bda2ca52b443b9af1cd12d45bcb1824cd8c
improdutividade <156726836+improdutividade@users.noreply.github.com>
2024-01-27 14:21:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 from datetime import datetime
-from babel.numbers import format_currency
 
 def calcular_valor_aluguel(data_inicio, valor_aluguel_mensal):
     # Defina a data de início
     data_inicio = datetime.strptime(data_inicio, "%d/%m/%Y")
 
-    # Calcule a diferença em segundos entre a data de início e a data atual
-    segundos_decorridos = (datetime.now() - data_inicio).total_seconds()
+    # Calcule a diferença em milissegundos entre a data de início e a data atual
+    milissegundos_decorridos = (datetime.now() - data_inicio).total_seconds() * 1000
 
-    # Calcule o valor gasto considerando o valor por segundo
-    valor_gasto = segundos_decorridos * (valor_aluguel_mensal / (30 * 24 * 60 * 60))  # Valor mensal convertido para segundos
+    # Calcule o valor gasto considerando o valor por milissegundo
+    valor_gasto = milissegundos_decorridos * (valor_aluguel_mensal / (30 * 24 * 60 * 60 * 1000))  # Valor mensal convertido para milissegundos
 
     return valor_gasto
 
@@ -26,8 +25,5 @@ valor_aluguel_mensal = 1750.00
 # Calcule o valor gasto com aluguel
 valor_gasto = calcular_valor_aluguel(data_inicio_fixa, valor_aluguel_mensal)
 
-# Formate o valor como dinheiro
-valor_formatado = format_currency(valor_gasto, 'BRL', locale='pt_BR')
-
-# Exiba o valor formatado
-st.text(f"Valor gasto com aluguel: {valor_formatado}")
+# Exiba o valor formatado com 6 casas decimais
+st.text(f"Valor gasto com aluguel: R$ {valor_gasto:.6f}")
